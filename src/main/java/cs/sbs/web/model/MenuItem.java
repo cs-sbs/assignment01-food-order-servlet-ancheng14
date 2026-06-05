d654cee1892819d57e6d549ce246d989aef1d3ce
ancheng14 <anchengdashuaibi@qq.com>
2026-06-05 14:24:14 +0800

    111
    111

--- src/main/java/cs/sbs/web/model/MenuItem.java
+++ src/main/java/cs/sbs/web/model/MenuItem.java
@@ -1,13 +1,22 @@
 package cs.sbs.web.model;
 
 public class MenuItem {
-
+    private int id;
     private String name;
-    private int price;
+    private double price;
 
-    public MenuItem(String name, int price) {
+    public MenuItem(int id, String name, double price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
 
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return id + ". " + name + " - $" + price;
+    }
 }
--- src/main/java/cs/sbs/web/servlet/MenuListServlet.java
+++ src/main/java/cs/sbs/web/servlet/MenuListServlet.java
@@ -1,15 +1,38 @@
 package cs.sbs.web.servlet;
 
+import cs.sbs.web.model.MenuItem;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class MenuListServlet extends HttpServlet {
 
+    private static final ArrayList<MenuItem> menu = new ArrayList<>();
+
+    @Override
+    public void init() {
+        menu.add(new MenuItem(1, "Fried Rice", 8));
+        menu.add(new MenuItem(2, "Fried Noodles", 9));
+        menu.add(new MenuItem(3, "Burger", 10));
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws IOException {
 
-        resp.getWriter().println("TODO: implement menu list");
+        resp.setContentType("text/plain; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        String name = req.getParameter("name");
+
+        out.println("Menu List:\n");
+
+        for (MenuItem item : menu) {
+            if (name == null || item.getName().toLowerCase().contains(name.toLowerCase())) {
+                out.println(item);
+            }
+        }
     }
 }
